b7dd935a578701727c8a4ec48a33279b7ba89ab8
abdullapro2006 <abdullahmanafli38@gmail.com>
2023-07-23 10:03:47 +0400

    Base entity class created

---
+++ C# final project/Data/Models/Sale.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using C__final_project.Data.Common;
+
+namespace C__final_project.Models
+{
+    public class Sale : BaseEntity 
+    {
+        private static int counter = 0;
+
+        public Sale(int amount,string saleitem,DateTime date)
+        {
+            Amount = amount;
+            SaleItem = saleitem;
+            Date = date;
+            ID = counter;
+            counter++;
+
+        }
+        public int Amount { get; set; }
+        public string SaleItem { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ C# final project/Data/Models/SaleItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using C__final_project.Data.Common;
+
+namespace C__final_project.Models
+{
+    public class SaleItem : BaseEntity
+    {
+        
+    }
+}
