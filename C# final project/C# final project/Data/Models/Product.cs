074f5a1f483562736c01246d27598cf9725942ce
abdullapro2006 <135731252+abdullapro2006@users.noreply.github.com>
2023-07-23 11:00:15 +0400

    Add files via upload
    Category enum created

---
+++ C# final project/C# final project/Data/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using C__final_project.Data.Common;
+
+namespace C__final_project.Models
+{
+
+    public class Product : BaseEntity
+    {
+        private  int counter = 0;
+
+
+        public Product(string name,double price,int count)
+        {
+            Name = name;
+            Price = price;
+            Count = count;
+            ID = counter;
+            count++;
+        }
+
+
+        public string  Name { get; set; }
+        public double Price { get; set; }
+        public int Count { get; set; }
+    }
+}
---
+++ C# final project/C# final project/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+       Console.Clear();
+        int option;
+
+
+        do
+        {
+            Console.WriteLine("1. To carry out operations on products");
+            Console.WriteLine("2. To carry out operations on sales");
+            Console.WriteLine("0. Exit from system");
+
+
+
+            Console.WriteLine("-----------------");
+            Console.WriteLine("Please enter an option");
+            Console.WriteLine("-----------------");
+
+            while(!int.TryParse(Console.ReadLine(), out option)) 
+            {
+                Console.WriteLine("Invalid option!");
+                Console.WriteLine("Enter an option please!");
+                Console.WriteLine("-----------------");
+
+
+                switch (option)
+                {
+                    case 1:
+
+                    default:
+                        Console.WriteLine("No such option!");
+                        break;
+                }
+
+            }
+
+        } while (option != 0);
+    }
+}
